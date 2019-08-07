1ce60d849bba0bababa68661eba123315c650cc7
Ninisbeth Segovia Noguera <ninisbeth_segovia@hotmail.com>
2019-08-06 23:21:25 -0500

    Datos relacionados con carga de datos diligente y explicita de EF Core

---
+++ Pages/Courses/Index.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversity.Models;
+using ContosoUniversity.Models.SchoolViewModels;
+
+namespace ContosoUniversity.Pages.Courses
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ContosoUniversity.Models.SchoolContext _context;
+
+        public IndexModel(ContosoUniversity.Models.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IList<CourseViewModel> CourseVM { get; set; }
+
+        public IList<Course> Course { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Course = await _context.Courses
+             .Include(c => c.Department)
+             .AsNoTracking()
+             .ToListAsync();
+
+            CourseVM = await _context.Courses
+            .Select(p => new CourseViewModel
+            {
+                CourseID = p.CourseID,
+                Title = p.Title,
+                Credits = p.Credits,
+                DepartmentName = p.Department.Name
+            }).ToListAsync();
+        }
+    }
+}
